b2648c6c0d288fb083d60a0d7fe7a3f939356081
Polina <s119m@yandex.com>
2022-04-24 00:25:28 +0300

    map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-console.log('hi');
+let card = document.getElementById("ebrd");
+console.log(card);
+let flag = true;
+let cardDiv;
+ebrd.onclick = function () {
+    if (cardDiv) {
+        card.removeChild(cardDiv);
+    }
+    cardDiv = document.createElement("div");
+    cardDiv.classList.add("bank-desc");
+    card.appendChild(cardDiv);
+    card.textContent += 'test';
+    console.log(card.childNodes);
+}
